9fee5627e42f7776ebd748cf5e45bb083ff42dc9
EkaterinaKorotkova <e.korotkova.2013@yandex.ru>
2022-05-05 23:07:58 +0300

    decision in general (with big cost)

--- Otus.LuckyNumbers.FirstHomework/Program.cs
+++ Otus.LuckyNumbers.FirstHomework/Program.cs
@@ -1,36 +1,45 @@
 ﻿using System;
+using System.Linq;
 
-namespace Otus.LuckyNumbers.FirstHomework
+namespace Otus
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var counter = 0;
-            for (var a1 = 0; a1 <= 9; a1++)
+            for (var i = 1; i <= 5; i++)
             {
-                for (var a2 = 0; a2 <= 9; a2++)
-                {
-                    for (var a3 = 0; a3 <= 9; a3++)
-                    {
-                        var sumA = a1 + a2 + a3;
+                var luckyNumbers = GetLuckyNumbers(i);
 
-                        for (var b1 = 0; b1 <= 9; b1++)
-                        {
-                            for (var b2 = 0; b2 <= 9; b2++)
-                            {
-                                var b = sumA - b1 - b2;
-                                if (b >= 0 && b <= 9)
-                                    counter++;
-                            }
-                        }
-                    }
+                Console.WriteLine($"Lucky numbers for {i}: {luckyNumbers}");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int GetLuckyNumbers(int n)
+        {
+            var maxNumber = Math.Pow(10, n);
+
+            var counter = 0;
+            for (var i = 0; i < maxNumber; i++)
+            {
+                for (var j = 0; j < maxNumber; j++)
+                {
+                    var sumA = Sum(i);
+                    var sumB = Sum(j);
+
+                    if (sumA == sumB)
+                        counter++;
                 }
             }
 
-            Console.WriteLine(counter);
+            return counter;
+        }
 
-            Console.ReadKey();
+        private static int Sum(int number)
+        {
+            return number.ToString().Sum(x => x - '0');
         }
     }
 }
