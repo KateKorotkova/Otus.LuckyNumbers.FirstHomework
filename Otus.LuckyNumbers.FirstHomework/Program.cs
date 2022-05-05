ca431279c94954f18699da995e124e87f40fab2b
EkaterinaKorotkova <e.korotkova.2013@yandex.ru>
2022-05-05 22:05:18 +0300

    added basic logic for junior level

--- Otus.LuckyNumbers.FirstHomework/Program.cs
+++ Otus.LuckyNumbers.FirstHomework/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Otus.LuckyNumbers.FirstHomework
 {
@@ -10,6 +6,31 @@ namespace Otus.LuckyNumbers.FirstHomework
     {
         static void Main(string[] args)
         {
+            var counter = 0;
+            for (var a1 = 0; a1 <= 9; a1++)
+            {
+                for (var a2 = 0; a2 <= 9; a2++)
+                {
+                    for (var a3 = 0; a3 <= 9; a3++)
+                    {
+                        var sumA = a1 + a2 + a3;
+
+                        for (var b1 = 0; b1 <= 9; b1++)
+                        {
+                            for (var b2 = 0; b2 <= 9; b2++)
+                            {
+                                var b = sumA - b1 - b2;
+                                if (b >= 0 && b <= 9)
+                                    counter++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+
+            Console.ReadKey();
         }
     }
 }
